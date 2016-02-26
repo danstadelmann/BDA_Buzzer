a4317d85d0512ef7a57b7ffc14b18495ada7389b
flaviok <flavio.kreiliger@bluewin.ch>
2016-02-26 20:16:49 +0100

    added radio suport. and added blocktime

--- Server/FRDM-KL25Z_MusicMaker/Sources/Shell.c
+++ Server/FRDM-KL25Z_MusicMaker/Sources/Shell.c
@@ -13,71 +13,77 @@
 #include "FAT1.h"
 #include "VS1053.h"
 #include "player.h"
-/*
-  #include "RApp.h"
-  #include "RNet_App.h"
-  #include "RNetConf.h"
-  #include "RStdIO.h"
 
-*/
-static const CLS1_ParseCommandCallback CmdParserTable[] =
-{
-  CLS1_ParseCommand,
+#if RADIO
+#include "RApp.h"
+#include "RNet_App.h"
+#include "RNetConf.h"
+#include "RStdIO.h"
+#endif
+
+static const CLS1_ParseCommandCallback CmdParserTable[] = { CLS1_ParseCommand,
 #if FRTOS1_PARSE_COMMAND_ENABLED
-  FRTOS1_ParseCommand,
+		FRTOS1_ParseCommand,
 #endif
 #if FAT1_PARSE_COMMAND_ENABLED
-  FAT1_ParseCommand,
+		FAT1_ParseCommand,
 #endif
-  VS_ParseCommand,
+		VS_ParseCommand,
 #if PLAYER
-  PLR_ParseCommand,
+		PLR_ParseCommand,
+#endif
+#if TmDt1_PARSE_COMMAND_ENABLED
+		TmDt1_ParseCommand,
 #endif
 #if RADIO
-  RNET1_ParseCommand,
-  RNETA_ParseCommand,
+		RNET1_ParseCommand, RNETA_ParseCommand,
 #endif
-  NULL /* sentinel */
+		NULL /* sentinel */
 };
 
 static portTASK_FUNCTION(ShellTask, pvParameters) {
-  bool cardMounted = FALSE;
-  static FAT1_FATFS fileSystemObject;
-  unsigned char buf[48];
+	bool cardMounted = FALSE;
+	static FAT1_FATFS fileSystemObject;
+	unsigned char buf[48];
 #if RADIO
-  static unsigned char radio_cmd_buf[48];
-  radio_cmd_buf[0] = '\0';
-  CLS1_ConstStdIOType *ioRemote = RSTDIO_GetStdioRx();
+	static unsigned char radio_cmd_buf[48];
+	radio_cmd_buf[0] = '\0';
+	CLS1_ConstStdIOType *ioRemote = RSTDIO_GetStdioRx();
 
 #endif
-  CLS1_ConstStdIOTypePtr ioLocal = CLS1_GetStdio();
+	CLS1_ConstStdIOTypePtr ioLocal = CLS1_GetStdio();
 
-  (void)pvParameters; /* not used */
-  buf[0] = '\0';
-  (void)CLS1_ParseWithCommandTable((unsigned char*)CLS1_CMD_HELP, ioLocal, CmdParserTable);
-  FAT1_Init();
-  for(;;) {
-    (void)FAT1_CheckCardPresence(&cardMounted,
-        "0" /* drive */, &fileSystemObject, CLS1_GetStdio());
-    if (cardMounted) {
-      LEDG_On();
-      LEDR_Off();
-    } else {
-      LEDG_Off();
-      LEDR_On();
-    }
-    (void)CLS1_ReadAndParseWithCommandTable(buf, sizeof(buf), CLS1_GetStdio(), CmdParserTable);
+	(void) pvParameters; /* not used */
+	buf[0] = '\0';
+	(void) CLS1_ParseWithCommandTable((unsigned char*) CLS1_CMD_HELP, ioLocal,
+			CmdParserTable);
+	FAT1_Init();
+	for (;;) {
+		(void) FAT1_CheckCardPresence(&cardMounted, "0" /* drive */,
+				&fileSystemObject, CLS1_GetStdio());
+		if (cardMounted) {
+			LEDG_On();
+			LEDR_Off();
+		} else {
+			LEDG_Off();
+			LEDR_On();
+		}
+		(void) CLS1_ReadAndParseWithCommandTable(buf, sizeof(buf),
+				CLS1_GetStdio(), CmdParserTable);
 #if RADIO
-    RSTDIO_Print(ioLocal); /* dispatch incoming messages */
-    (void)CLS1_ReadAndParseWithCommandTable(radio_cmd_buf, sizeof(radio_cmd_buf), ioRemote, CmdParserTable);
+		RSTDIO_Print(ioLocal); /* dispatch incoming messages */
+		(void) CLS1_ReadAndParseWithCommandTable(radio_cmd_buf,
+				sizeof(radio_cmd_buf), ioRemote, CmdParserTable);
 #endif
-    FRTOS1_vTaskDelay(50/portTICK_RATE_MS);
-  }
+		FRTOS1_vTaskDelay(50/portTICK_RATE_MS);
+	}
 }
 
 void SHELL_Init(void) {
-  if (FRTOS1_xTaskCreate(ShellTask, "Shell", configMINIMAL_STACK_SIZE+200, NULL, tskIDLE_PRIORITY, NULL) != pdPASS) {
-    for(;;){} /* error */
-  }
+	if (FRTOS1_xTaskCreate(ShellTask, "Shell", configMINIMAL_STACK_SIZE+200,
+			NULL, tskIDLE_PRIORITY, NULL) != pdPASS) {
+		for (;;) {
+		} /* error */
+	}
 }
 
